2610eb03297593140bc1a770f3e5e602a8f9227e
Jefry Chiedi <jefrychiedi1999@gmail.com>
2023-07-18 13:58:22 +0700

    setup iis

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"ebapp-api-dev/config"
 	"ebapp-api-dev/modules/auth"
 	"ebapp-api-dev/modules/boqbody"
+	"log"
+	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	cors "github.com/rs/cors/wrapper/gin"
@@ -22,5 +25,16 @@ func main() {
 
 	boqbody.NewBoqBodyHandler(v1, boqbody.BoqBodyRegistry(db))
 
-	router.Run(":88")
+	// Mengatur mode GIN menjadi release
+	gin.SetMode(gin.ReleaseMode)
+
+	port := "88"
+	if os.Getenv("ASPNETCORE_PORT") != "" {
+		port = os.Getenv("ASPNETCORE_PORT")
+	}
+
+	err := http.ListenAndServe(":"+port, router)
+	if err != nil {
+		log.Fatal("Gagal mendengarkan port "+port+":", err)
+	}
 }
