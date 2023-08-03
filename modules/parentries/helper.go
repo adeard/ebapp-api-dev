3480920dcb5392133e6a455d14a4517bf6f4d58a
Jefry Chiedi <jefrychiedi1999@gmail.com>
2023-08-03 14:58:05 +0700

    init

--- modules/parentries/helper.go
+++ modules/parentries/helper.go
@@ -1,5 +1,19 @@
 package parentries
 
+import "github.com/gin-gonic/gin"
+
 type parEntriesHandler struct {
 	parEntriesService Service
+}
+
+func NewParEntriesHandler(v1 *gin.RouterGroup, parEntriesService Service) {
+	handler := &parEntriesHandler{parEntriesService}
+
+	parEntries := v1.Group("par_entries")
+
+	parEntries.GET("", handler.GetAll)
+}
+
+func (h *parEntriesHandler) GetAll(c *gin.Context) {
+
 }
