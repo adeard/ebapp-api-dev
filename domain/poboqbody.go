ed91a2e36590c6cf89d617c90943b7f9a3f6b719
Jefry Chiedi <jefrychiedi1999@gmail.com>
2023-09-05 16:29:41 +0700

    init

---
+++ domain/poboqbody.go
@@ -0,0 +1,47 @@
+package domain
+
+type PoBoqBody struct {
+	Id                int     `json:"id" gorm:"column:id"`
+	ParentId          int     `json:"parent_id" gorm:"parent_id"`
+	RunNum            string  `json:"run_num" gorm:"column:run_num"`
+	ItemNo            string  `json:"item_no" gorm:"column:item_no"`
+	ItemLevel         int     `json:"item_level" gorm:"column:item_level"`
+	ItemDescription   string  `json:"item_description" gorm:"column:item_description"`
+	ItemSpecification string  `json:"item_specification" gorm:"column:item_specification"`
+	Qty               float32 `json:"qty" gorm:"column:qty"`
+	Unit              string  `json:"unit" gorm:"column:unit"`
+	Price             float64 `json:"price" gorm:"column:price"`
+	Currency          string  `json:"currency" gorm:"column:currency"`
+	Note              string  `json:"note" gorm:"column:note"`
+}
+
+type PoBoqBodyRequest struct {
+	Id                int     `json:"id"`
+	ParentId          int     `json:"parent_id"`
+	RunNum            string  `json:"run_num"`
+	ItemNo            string  `json:"item_no"`
+	ItemLevel         int     `json:"item_level"`
+	ItemDescription   string  `json:"item_description"`
+	ItemSpecification string  `json:"item_specification"`
+	Qty               float32 `json:"qty"`
+	Unit              string  `json:"unit"`
+	Price             float64 `json:"price"`
+	Currency          string  `json:"currency"`
+	Note              string  `json:"note"`
+}
+
+type PoBoqBodyResponse struct {
+	Id                int                 `json:"id"`
+	ParentId          int                 `json:"parent_id"`
+	RunNum            string              `json:"run_num"`
+	ItemNo            string              `json:"item_no"`
+	ItemLevel         int                 `json:"item_level"`
+	ItemDescription   string              `json:"item_description"`
+	ItemSpecification string              `json:"item_specification"`
+	Qty               float32             `json:"qty"`
+	Unit              string              `json:"unit"`
+	Price             float64             `json:"price"`
+	Currency          string              `json:"currency"`
+	Note              string              `json:"note"`
+	Children          []PoBoqBodyResponse `json:"children"`
+}
