dbf737b6b7d9c0645cf43324f4fb937fff24c2dc
ramadhancoba123 <ramadhancoba123@gmail.com>
2024-05-13 11:36:35 +0700

    clone body progress to body cpp

--- modules/poboqheadercpp/repository.go
+++ modules/poboqheadercpp/repository.go
@@ -10,6 +10,7 @@ type Repository interface {
 	FindCpp(id string) ([]domain.PoBoqHeaderCpp, error)
 	Store(input domain.PoBoqHeaderCpp) (domain.PoBoqHeaderCpp, error)
 	Delete(id string) error
+	CloneProgress(oldIdProgress string, newIdCpp string) error
 }
 
 type repository struct {
@@ -42,4 +43,45 @@ func (r *repository) Store(input domain.PoBoqHeaderCpp) (domain.PoBoqHeaderCpp, 
 func (r *repository) Delete(id string) error {
 	err := r.db.Table("po_boq_header_cpp").Where("pekerjaan_no =?", id).Delete(&domain.PoBoqHeaderCpp{}).Error
 	return err
+}
+
+// Clone CPP body from header
+func (r *repository) CloneProgress(oldIdProgress string, newIdCpp string) error {
+	// Lakukan query untuk meng-INSERT data baru berdasarkan data yang ada dengan menggunakan parameter oldId dan newId
+	query := `INSERT INTO eBAPP.dbo.po_boq_body_cpp ([run_num]
+		,[item_no]
+		,[item_level]
+		,[item_description]
+		,[item_specification]
+		,[qty]
+		,[status]
+		,[note]
+		,[main_id]
+		,[parent_id]
+		,[order])
+               SELECT 
+                   ? AS run_num,
+                   item_no,
+                   item_level,
+                   item_description,
+                   item_specification,
+                   qty,
+                   0,
+                   null,
+				   main_id,
+                   parent_id,
+				   [order]
+               FROM eBAPP.dbo.po_boq_body_progress
+               WHERE run_num = ?`
+
+	// Eksekusi query dengan parameter oldId dan newId
+	result := r.db.Exec(query, newIdCpp, oldIdProgress)
+
+	// Periksa jika terjadi kesalahan saat menjalankan query
+	if result.Error != nil {
+		return result.Error
+	}
+
+	// Jika berhasil, kembalikan pesan berhasil
+	return nil
 }
--- modules/poboqheadercpp/service.go
+++ modules/poboqheadercpp/service.go
@@ -23,6 +23,9 @@ func (s *service) GetCpp(id string) ([]domain.PoBoqHeaderCpp, error) {
 
 func (s *service) Store(input domain.PoBoqHeaderCpp) (domain.PoBoqHeaderCpp, error) {
 	headers, err := s.repository.Store(input)
+	if err == nil {
+		s.repository.CloneProgress(input.PekerjaanNoProgress, input.PekerjaanNo)
+	}
 	return headers, err
 }
 
